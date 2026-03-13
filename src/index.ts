c795897cea234c48350927aa0d97b8c263d9417e
Rafael dos Santos Giorgi <106035150+devraffles@users.noreply.github.com>
2026-03-13 11:17:39 -0300

    chore: adição do fastify swagger e geração da documentação

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import "dotenv/config";
 
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUI from "@fastify/swagger-ui";
 import Fastify from "fastify";
 import {
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider,
@@ -15,8 +18,25 @@ const app = Fastify({
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
 
-app.get("/", async function handler() {
-  return { hello: "world" };
+await app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Bootcamp Treinos API",
+      description: "API para o bootcamp de treinos do FSC",
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        description: "Localhost",
+        url: "http://localhost:8081",
+      },
+    ],
+  },
+  transform: jsonSchemaTransform,
+});
+
+await app.register(fastifySwaggerUI, {
+  routePrefix: "/docs",
 });
 
 app.withTypeProvider<ZodTypeProvider>().route({
